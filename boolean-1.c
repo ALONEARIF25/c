108b579767a681d9fe08fa06692fd705d3704006
Arif <arifkhan408760@gmail.com>
2025-07-15 18:03:40 +0600

    Create boolean-1.c

---
+++ boolean-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+  bool sabaSundor = true;
+  
+  if(sabaSundor) {
+      printf("you are on the right path!");
+  } else if(!sabaSundor) {
+      printf("oops...");
+  }
+
+}
